36a9af348931f8950d2dced8d99e0496d2645b9c
Aashish <office.hyundai.aashish@gmail.com>
2026-06-05 00:56:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "gst-calculator-v2"; // Increment cache version
+const CACHE_NAME = "gst-calculator-v3"; // Increment cache version for new changes
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./service-worker.js", // Added service worker itself to cache
   "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap",
   "icons/icon-192.png",
   "icons/icon-512.png"
